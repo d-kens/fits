ed2c1826e05b569020ea4d4c4d81f67af1d87edc
d-kens <omondikens1@gmail.com>
2024-01-30 06:11:09 +0300

    admin panel bootstrap setup

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layout.admin')
+
+@section('title', 'Analytics')
+
+@section('contents')
+
+    <div class="row" style="border: red solid 5px">
+        Welcome to the admin dashboard
+    </div>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>fits</title>
+
+    <style>
+        body {
+            display: grid;
+            height: 100vh;
+            place-content: center;
+        }
+    </style>
+</head>
+<body>
+
+    <h2>
+        fits ecommerce store. Comming Sooon!!!
+    </h2>
+
+</body>
+</html>
