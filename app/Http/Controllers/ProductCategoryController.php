352460542ac3c6f34e3ee274457a37ec2d2d7505
d-kens <omondikens1@gmail.com>
2024-01-30 08:25:01 +0300

    README.md

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    //
+    public function index() {
+
+    }
+
+    public function show() {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy() {
+        
+    }
+}
